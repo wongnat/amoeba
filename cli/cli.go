681163dd186b26687e637efcc6e6ae4aea13ac55
Nathan Wong <nathan_wong@live.com>
2017-10-21 23:16:14 -0700

    fix format

--- cli/cli.go
+++ cli/cli.go
@@ -1,61 +1,61 @@
 package main
 
 import (
-    "os"
-    "io"
-    "fmt"
+	"amoeba/amoeba"
+	"fmt"
+	"io"
 	"log"
-    "amoeba/amoeba"
+	"os"
 )
 
 func main() {
-    var url string
-    var sha string
-    var dir string
+	var url string
+	var sha string
+	var dir string
 
-    args := os.Args
-    n    := len(args)
+	args := os.Args
+	n := len(args)
 
-    if n != 4 {
-        fmt.Println("Usage: amoeba <build-dir> <ssh-url> <commit-sha>")
-        return
-    }
-
-    url = args[1]
-    sha = args[2]
-    dir = args[3]
-
-    a, err := amoeba.NewAmoeba(url, sha, dir)
-    if err != nil {
-		log.Fatal(err)
+	if n != 4 {
+		fmt.Println("Usage: amoeba <build-dir> <ssh-url> <commit-sha>")
+		return
 	}
-    defer a.Close()
 
-    outputs, err := a.Start()
+	url = args[1]
+	sha = args[2]
+	dir = args[3]
+
+	a, err := amoeba.NewAmoeba(url, sha, dir)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-    for _, output := range outputs {
-        io.Copy(os.Stdout, output.Stdout)
-    }
+	defer a.Close()
 
-    errs, err := a.Wait()
+	outputs, err := a.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-    passed := true
-    for _, err = range errs {
-        if err != nil {
-            passed = false
-            break
-        }
-    }
 
-    if passed {
-        fmt.Println("PASSED")
-    } else {
-        fmt.Println("FAILED")
-    }
+	for _, output := range outputs {
+		io.Copy(os.Stdout, output.Stdout)
+	}
+
+	errs, err := a.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	passed := true
+	for _, err = range errs {
+		if err != nil {
+			passed = false
+			break
+		}
+	}
+
+	if passed {
+		fmt.Println("PASSED")
+	} else {
+		fmt.Println("FAILED")
+	}
 }
--- server/server.go
+++ server/server.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-    "io"
-    "os"
-    "log"
-    "sync"
-    "strconv"
-    "net/http"
-    "encoding/json"
-    "amoeba/amoeba"
-    "github.com/gorilla/websocket"
-    "github.com/gorilla/mux"
+	"amoeba/amoeba"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
 )
 
 const defaultMaxBuilds = 8
@@ -25,168 +25,169 @@ var upgrader = websocket.Upgrader{} // upgrade http conn to websocket
 var buildsDir string
 
 func main() {
-    args   := os.Args
-    length := len(args)
+	args := os.Args
+	length := len(args)
 
-    if length < 3 || length > 4 {
-        log.Fatal("Error: unexpected number of arguments")
-    }
+	if length < 3 || length > 4 {
+		log.Fatal("Error: unexpected number of arguments")
+	}
 
-    if length == 4 {
-        num, err := strconv.ParseInt(args[3], 10, 64)
-        if err != nil {
+	if length == 4 {
+		num, err := strconv.ParseInt(args[3], 10, 64)
+		if err != nil {
 			log.Fatal(err)
 		}
-        maxBuilds = num
-    } else {
-        maxBuilds = defaultMaxBuilds
-    }
+		maxBuilds = num
+	} else {
+		maxBuilds = defaultMaxBuilds
+	}
 
-    builds = newOutputMap()
-    buildsDir = args[1]
+	builds = newOutputMap()
+	buildsDir = args[1]
 
-    router := mux.NewRouter()
-    router.HandleFunc("/build", handleBuild)
-    router.HandleFunc("/build/ids", handleIds)
-    router.HandleFunc("/build/{id}/clients", handleClients)
-    router.HandleFunc("/build/{id}/{client}/stdout", handleOutput)
+	router := mux.NewRouter()
+	router.HandleFunc("/build", handleBuild)
+	router.HandleFunc("/build/ids", handleIds)
+	router.HandleFunc("/build/{id}/clients", handleClients)
+	router.HandleFunc("/build/{id}/{client}/stdout", handleOutput)
 
-    port := args[2]
-    log.Println("Amoeba server listening on port " + port + " ...")
-    http.ListenAndServe(":" + port, router)
+	port := args[2]
+	log.Println("Amoeba server listening on port " + port + " ...")
+	http.ListenAndServe(":"+port, router)
 }
 
 // Serves json list of build ids
 func handleIds(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodGet {
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-    bids := builds.TopKeys()
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    json.NewEncoder(w).Encode(bids)
+	bids := builds.TopKeys()
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(bids)
 }
 
 // Serves json list of client names given the build id
 func handleClients(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodGet {
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-    vars := mux.Vars(r)
-    id   := vars["id"]
+	vars := mux.Vars(r)
+	id := vars["id"]
 
-    clients := builds.BotKeys(id)
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    json.NewEncoder(w).Encode(clients)
+	clients := builds.BotKeys(id)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(clients)
 }
 
 // Serves websocket of docker-compose stdout for client repo.
 func handleOutput(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    id   := vars["id"]
-    cli  := vars["client"]
+	vars := mux.Vars(r)
+	id := vars["id"]
+	cli := vars["client"]
 
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        return
-    }
-    defer conn.Close()
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		return
+	}
+	defer conn.Close()
 
-    out, ok := builds.Load(id, cli)
-    if !ok {
-        return
-    }
+	out, ok := builds.Load(id, cli)
+	if !ok {
+		return
+	}
 
-    copy(conn, out.Stdout)
+	copy(conn, out.Stdout)
 }
 
 // Intended as a github push/pr event.
 func handleBuild(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
-        w.WriteHeader(http.StatusBadRequest)
-        return
-    }
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
-    var jsonIn map[string]interface{}
+	var jsonIn map[string]interface{}
 
-    dec := json.NewDecoder(r.Body)
-    dec.Decode(&jsonIn)
+	dec := json.NewDecoder(r.Body)
+	dec.Decode(&jsonIn)
 
-    headCommit := jsonIn["head_commit"].(map[string]interface{})
-    repository := jsonIn["repository"].(map[string]interface{})
+	headCommit := jsonIn["head_commit"].(map[string]interface{})
+	repository := jsonIn["repository"].(map[string]interface{})
 
-    name := repository["name"].(string)
-    sha  := headCommit["id"].(string)
-    url  := repository["ssh_url"].(string)
-    bid  := name + "-" + sha
+	name := repository["name"].(string)
+	sha := headCommit["id"].(string)
+	url := repository["ssh_url"].(string)
+	bid := name + "-" + sha
 
-    log.Println("Received request to test: " + bid)
+	log.Println("Received request to test: " + bid)
 
-    a, err := amoeba.NewAmoeba(url, sha, buildsDir)
-    if err != nil {
-        w.Header().Set("Content-Type", "text/plain")
-        w.WriteHeader(http.StatusInternalServerError)
-        io.WriteString(w, err.Error())
-        return
-    }
-    defer a.Close()
+	a, err := amoeba.NewAmoeba(url, sha, buildsDir)
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusInternalServerError)
+		io.WriteString(w, err.Error())
+		return
+	}
+	defer a.Close()
 
-    for { // Block until the number of in-progress builds is < maxBuilds
-        countMu.Lock()
-        if count < maxBuilds {
-            count++
-            countMu.Unlock()
-            break
-        }
-        countMu.Unlock()
-    }
+	for { // Block until the number of in-progress builds is < maxBuilds
+		countMu.Lock()
+		if count < maxBuilds {
+			count++
+			countMu.Unlock()
+			break
+		}
 
-    outputs, err := a.Start()
+		countMu.Unlock()
+	}
+
+	outputs, err := a.Start()
 	if err != nil { // at least one user repo was setup incorrectly
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
 		io.WriteString(w, err.Error())
-        return
+		return
 	}
-	
-    val := make(map[string]amoeba.ComposeOutput)
-    for _, output := range outputs {
-        val[output.RepoName] = output
-    }
 
-    builds.Insert(bid, val)
-    errs, err := a.Wait()
+	val := make(map[string]amoeba.ComposeOutput)
+	for _, output := range outputs {
+		val[output.RepoName] = output
+	}
+
+	builds.Insert(bid, val)
+	errs, err := a.Wait()
 	if err != nil {
 		w.Header().Set("Content-Type", "text/plain")
 		w.WriteHeader(http.StatusOK)
 		io.WriteString(w, err.Error())
-        return
+		return
 	}
-	
-    passed := true
-    for _, err = range errs {
-        if err != nil {
-            passed = false
-            break
-        }
-    }
 
-    w.Header().Set("Content-Type", "text/plain")
-    w.WriteHeader(http.StatusOK)
-    if passed {
-        io.WriteString(w, "PASSED")
-    } else {
-        io.WriteString(w, "FAILED")
-    }
+	passed := true
+	for _, err = range errs {
+		if err != nil {
+			passed = false
+			break
+		}
+	}
 
-    countMu.Lock()
-    count--
-    countMu.Unlock()
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	if passed {
+		io.WriteString(w, "PASSED")
+	} else {
+		io.WriteString(w, "FAILED")
+	}
 
-    log.Println("Finished testing: " + bid)
+	countMu.Lock()
+	count--
+	countMu.Unlock()
+
+	log.Println("Finished testing: " + bid)
 }
